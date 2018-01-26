552526b7d5d605273f8c2945aef6d5fb6a7bd238
RyanGan <ryan.william.gan@gmail.com>
2018-01-26 17:07:24 -0500

    updated title

---
+++ r_scripts/outcome_wrangle/2010-2015_morbidity_colorado_county.R
@@ -0,0 +1,10 @@
+# ------------------------------------------------------------------------------
+# Title: Data wrangling and creation of county time series counts of 
+#   cardiopulmonary emergency department visits in Colorado state 2010-2015
+# Author: Ryan Gan
+# Date Created: 2018-01-25
+# ------------------------------------------------------------------------------
+
+# libraries ----
+# loading tidyverse
+library(tidyverse)
