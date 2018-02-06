f12a450cab5b98a57416430da84fbb18bf7a634b
RyanGan <ryan.william.gan@gmail.com>
2018-02-06 11:17:28 -0500

    Created new script to create 2015 casecross over dataframes from Washington state.

---
+++ r_scripts/outcome_wrangle/2015_morbidity_washington_casecross.R
@@ -0,0 +1,8 @@
+# ------------------------------------------------------------------------------
+# Title: Time-stratified case-crossover dataframes for inpatient emergency 
+#        department hospitalizations 
+# Author: Ryan Gan
+# Date Created: 2018-02-06
+# ------------------------------------------------------------------------------
+
+# I think I'll run this on the server; maybe in parallel
