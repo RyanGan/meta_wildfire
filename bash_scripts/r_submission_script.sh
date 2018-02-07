c26e3301f28463c49bcb20d3c26df84b5887d22b
RyanGan <ryan.william.gan@gmail.com>
2018-02-07 13:08:46 -0500

    added a bash script to run on ozone server

---
+++ bash_scripts/r_submission_script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#PBS -N ryan_rjob
+#PBS -l nodes=1:ppn=1
+#PBS -W group_list=pierce_group
+#PBS -q batch
+#PBS -M rgan@colostate.edu
+#PBS -m abe
+
+# set memory to unlimited
+ulimit -s unlimited
+
+# set command directory
+cd /home/ryangan/local_git_repo/meta_wildfire/
+
+# export R library
+export R_LIB=~/R/x86_64-pc-linux-gnu-library/3.4
+
+# run rscript
+Rscript --vanilla ./r_scripts/analysis/2015-morbidity_dl_ts_analyses.R
+
