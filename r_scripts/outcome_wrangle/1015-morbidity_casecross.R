945a97b795abec30ce01a76defa5e458e5c2c188
RyanGan <ryan.william.gan@gmail.com>
2018-03-13 13:36:25 -0400

    created script to create time-stratified case crossover dataframe for morbidity.

---
+++ r_scripts/outcome_wrangle/1015-morbidity_casecross.R
@@ -0,0 +1,35 @@
+# ------------------------------------------------------------------------------
+# Title: Creation of morbidity case-crossover dataframe for CO, WA, OR 2010-2015
+# Author: Ryan Gan
+# Date Created: 2018-03-12
+# ------------------------------------------------------------------------------
+
+# This script combines estimated population-weighted PM2.5, temperature, HMS,
+# and air quality index data to create a time series from 2010 to 2015.
+# I am considering extending this to 2006 to 2010; I would need to process
+# temperature data.
+
+# library ----
+library(tidyverse)
+
+# read ER hosp
+er_hosp <- read_csv("./data/health/er_hosp.csv")
+
+# apply time stratified case-crossover function ----
+# time-stratified case-crossover ----
+start_time <- Sys.time()
+casecross_list <- icd9_outcomes %>% 
+  # create lists of outcome dataframes
+  map(~filter(er_hosp, dx1 %in% .)) %>% 
+  # apply ts casecross over function and join with pm data
+  map(~casecross(data = ., id = "id", date = "date", period = "month", 
+    covariate = c("stay_length", "dx1", "age", "age_cat", "sex", "zip", 
+                  "fips", "state"))) 
+stop_time <- Sys.time()
+time <- stop_time - start_time
+time
+
+# saving casecross over list as Rdata
+save(casecross_list, file = paste0("./data/health/",
+  "1015-morbidity_casecross_list.RData"))
+
